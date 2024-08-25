52bdfb9cf80efee6207ec717a862f52400ea9ba7
Srinivasa K <153788570+Srinivasak4512-dev@users.noreply.github.com>
2024-08-25 11:42:29 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-eks-bucket-terraform"
+    bucket         = "my-eks-bucket-terraform1"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "dynamodb-state-lock"
